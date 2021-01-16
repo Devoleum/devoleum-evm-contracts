965380c01405aaaaab45bb31784803084c6ac673
lorenzozaccagnini <lorenzozcg@gmail.com>
2021-01-16 15:17:21 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = (props) => {
   useEffect(() => {
     setWeb3Task();
     console.log("fired");
-    setToken(JSON.parse(localStorage.getItem('userInfo')).token);
+    setToken(JSON.parse(localStorage.getItem("userInfo")).token);
   }, []);
 
   const setWeb3Task = async () => {
@@ -29,6 +29,54 @@ const App = (props) => {
   };
   return (
     <div>
+      <div>
+        <a
+          href="https://devoleum.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Devoleum
+        </a>{" "}
+        is a{" "}
+        <a
+          href="https://github.com/Devoleum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open source
+        </a>{" "}
+        web app that organizes data from physical or digital supply chains into
+        authentic stories. Thanks to Devoleum it is possible to notarize the
+        steps of a supply chain in an immutable way on blockchains and other
+        distributed systems, using cryptographic hashes that allow data
+        verification and a high degree of privacy. Each story shows in a clear
+        and detailed way the steps that contributed to making the product unique
+        and precious. Here you can verify the stories showed on our platform. It's possible to notarize steps if you have the permission.
+      </div>
+      <br />
+      <div>
+        In order to make it work you need to have the{" "}
+        <a
+          href="https://metamask.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Metamask browser extension (Rinkeby network)
+        </a>{" "}
+        and a CORS enabler extension{" "}
+        <a
+          href="https://addons.mozilla.org/en-US/firefox/addon/cors-everywhere/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          like CORS Everywhere
+        </a>
+        , we will fix that ASAP,         <a
+          href="https://github.com/Devoleum/devoleum-eth-notarization-muninn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >feel free to open an issue</a>)
+      </div>
       <h2>Verify</h2>
       <Verifier web3={web3} contract={contract} />
       <hr></hr>
