e72454addd721979872acb25c4cb608a9175577b
lorenzozaccagnini <lorenzozcg@gmail.com>
2021-06-01 21:02:01 +0200

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -15,8 +15,16 @@ const Header = () => (
         open source
       </a>{" "}
       web app that organizes linked open data from physical or digital supply
-      chains into authentic stories notarized on various blockchains. Here you can verify the stories showed on
-      our platform notarized on Ethereum.
+      chains into authentic stories notarized on various blockchains. Here you
+      can verify the stories showed on our platform notarized on Ethereum.
+    </div>
+    <br />
+    <div>
+      Donate to
+      <br />
+      ETH: 0x0F4cC51fd25E7E9954BBcc33efCB2A36B7Ab8904
+      <br />
+      ALGO: 5N22O3PIXAGNAGHBFSU6HQ22KGI4D3XEBACEFODVH3UOKCA4C2IBRD4ZDE
     </div>
     <br />
   </div>
